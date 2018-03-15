12f0f7b2fc2c8d8a4fa8212d65252261899ab453
Yung Dai <yungchidai@gmail.com>
2018-03-14 22:03:54 -0400

    Initial Commit

---
+++ LaskinFramework/LaskinFramework.h
@@ -0,0 +1,19 @@
+//
+//  LaskinFramework.h
+//  LaskinFramework
+//
+//  Created by Yung Dai on 2018-03-14.
+//  Copyright © 2018 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LaskinFramework.
+FOUNDATION_EXPORT double LaskinFrameworkVersionNumber;
+
+//! Project version string for LaskinFramework.
+FOUNDATION_EXPORT const unsigned char LaskinFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaskinFramework/PublicHeader.h>
+
+
